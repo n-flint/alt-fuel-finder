09dd1adbfad166a4b168b078d01c64ffb0e134af
Noah Flint <noah.flint24@gmail.com>
2019-05-13 11:09:39 -0600

    added get_json method to service

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -2,11 +2,17 @@ class NrelService
 
   def initialize(zip)
     @zip = zip
-    conn = Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=o19OOIgUGhWag1HnWn0eodouLREweb7hmve9Rr41&location=80206') do |f|
-      f.adapter Faraday.default_adapter
-    end
-    require 'pry'; binding.pry
+  end
 
+  def get_json
+    response = conn.get
+    json = JSON.parse(response.body)
+  end
+
+  def conn
+    test = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=o19OOIgUGhWag1HnWn0eodouLREweb7hmve9Rr41&location=80206") do |f|
+    f.adapter Faraday.default_adapter
+    end
   end
 
 
