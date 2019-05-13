a54c059c4cfc3b8a8f76cdc941af030384a3dd40
Noah Flint <noah.flint24@gmail.com>
2019-05-13 09:51:43 -0600

    added NrelFacade.new to index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
 
   def index
-
+    render locals: {
+      facade: NrelFacade.new(params[:q])
+    }
+    # require 'pry'; binding.pry
   end
 
 end
